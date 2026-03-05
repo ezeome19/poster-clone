5a38c87ee437db95f8ee6155bf1636efb558d5d8
Ezeome Ugo <ezeome19@gmail.com>
2026-03-05 01:07:03 +0100

    db issues

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -4,7 +4,15 @@ const config = require('config');
 
 module.exports = function () {
     const db = process.env.MONGO_URI || 'mongodb://localhost/poster-clone';
+
+    // Diagnostic logging (masks password)
+    const maskedDb = db.includes('@')
+        ? db.replace(/\/\/.*:.*@/, '//****:****@')
+        : db;
+
+    winston.info(`Attempting to connect to: ${maskedDb}`);
+
     mongoose.connect(db)
-        .then(() => winston.info(`Connected to ${db}...`))
+        .then(() => winston.info(`Connected to MongoDB successfully.`))
         .catch(err => winston.error(`Could not connect to MongoDB: ${err.message}`));
 };
