7a42f5610385310575ef0748e59118472011cebb
Ezeome Ugo <ezeome19@gmail.com>
2026-03-20 09:32:20 +0100

    .

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-const POSTER_CLONE_URL = 'https://poster-clone-eight.vercel.app'; // Production URL
+const POSTER_CLONE_URL = 'https://poster-clone.onrender.com'; // Production URL
 
 document.getElementById('openApp').addEventListener('click', () => {
     chrome.tabs.create({ url: POSTER_CLONE_URL + '/external' });
